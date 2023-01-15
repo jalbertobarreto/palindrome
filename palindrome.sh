1e0c79ac8203ef193b9ad586d04625c6a8937b07
jalbertobarreto <122553603+jalbertobarreto@users.noreply.github.com>
2023-01-15 16:22:14 -0600

    Update palindrome.sh

--- palindrome.sh
+++ palindrome.sh
@@ -4,8 +4,8 @@
 #writen by alberto barreto
 
 # Define variables
-palindrome-svr=$(gcloud compute instances list | grep INTERNAL_IP | awk -F: 'NR==2{print $2}' | sed 's/^ *//g')
+palindromesvr=$(gcloud compute instances list | grep palindrome-svr | awk '{print $4}')
 
 echo "The word to be tested is $1"
 echo "The results are saved in palindrome.txt"
-curl -s $palindrome-svr:81/palindrome/"$1" > palindrome.txt
+curl -s $palindromesvr:81/palindrome/"$1" > palindrome.txt
