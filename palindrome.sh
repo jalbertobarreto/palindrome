f89eb8316c549f9b347b5255d77cafa2e9e73693
jalbertobarreto <122553603+jalbertobarreto@users.noreply.github.com>
2023-01-15 13:11:11 -0600

    Update palindrome.sh

--- palindrome.sh
+++ palindrome.sh
@@ -5,4 +5,4 @@
 
 echo "The word to be tested is $1"
 echo "The results are saved in palindrome.txt"
-curl -s localhost:81/palindrome/"$1 > palindrome.txt
+curl -s localhost:81/palindrome/"$1" > palindrome.txt
