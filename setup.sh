ecfa860446a456e70685762086d91141fd6c4816
jalbertobarreto <122553603+jalbertobarreto@users.noreply.github.com>
2023-01-16 15:18:33 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,10 +13,15 @@ gcloud deployment-manager deployments create palindrome-deployment --config conf
 gcloud compute config-ssh --quiet && sleep 20
 
 # Define variables
-project=$(gcloud config list --format='text(core.project)' | awk -F: '{print $2}' | sed 's/^ *//') && palindromesvr=$(gcloud compute instances list | grep INTERNAL_IP | awk -F: 'NR==2{print $2}' | sed 's/^ *//g')
+project=$(gcloud config list --format='text(core.project)' | awk -F: '{print $2}' | sed 's/^ *//') \
+&& palindromesvr=$(gcloud compute instances list | grep INTERNAL_IP | awk -F: 'NR==2{print $2}' | sed 's/^ *//g')
 
 # Delete external nat from palindrome-svr
 gcloud compute instances delete-access-config --access-config-name="External NAT" --zone=us-central1-a palindrome-svr
 
 # Download palindrome script to bastion-host, then log in to it
-gcloud compute ssh --project=$project --zone=us-central1-a bastion-host -- "mkdir /home/$USER/palindrome && curl -s 'https://raw.githubusercontent.com/jalbertobarreto/palindrome/main/palindrome.sh' > /home/$USER/palindrome/palindrome.sh && chmod +x /home/$USER/palindrome/palindrome.sh && cd /home/$USER/palindrome && sed -i '7ipalindromesvr='"$palindromesvr" palindrome.sh ; bash"
+gcloud compute ssh --project=$project --zone=us-central1-a bastion-host -- "mkdir /home/$USER/palindrome \
+&& curl -s 'https://raw.githubusercontent.com/jalbertobarreto/palindrome/main/palindrome.sh' > /home/$USER/palindrome/palindrome.sh \]
+&& chmod +x /home/$USER/palindrome/palindrome.sh \
+&& cd /home/$USER/palindrome \
+&& sed -i '7ipalindromesvr='"$palindromesvr" palindrome.sh ; bash"
